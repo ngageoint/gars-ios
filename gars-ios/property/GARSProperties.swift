bda729633945ec44b889dfc946a630e329276506
Brian Osborn <bosborn@gmail.com>
2022-08-09 14:26:52 -0600

    properties

---
+++ gars-ios/property/GARSProperties.swift
@@ -0,0 +1,47 @@
+//
+//  GARSProperties.swift
+//  gars-ios
+//
+//  Created by Brian Osborn on 8/9/22.
+//
+
+import Foundation
+import grid_ios
+
+/**
+ * GARS property loader
+ */
+public class GARSProperties: GridProperties {
+    
+    /**
+     * Bundle Name
+     */
+    public static let BUNDLE_NAME = "gars-ios.bundle"
+    
+    /**
+     * Properties Name
+     */
+    public static let PROPERTIES_NAME = "gars"
+    
+    /**
+     * Singleton instance
+     */
+    private static let _instance = GARSProperties(bundle: BUNDLE_NAME, name: PROPERTIES_NAME)
+    
+    public static var instance: GARSProperties {
+        get {
+            return _instance
+        }
+    }
+    
+    /**
+     *  Initialize
+     *
+     *  @param bundle  bundle name
+     *  @param name    properties name
+     */
+    public override init(bundle: String, name: String) {
+        super.init(bundle: bundle, name: name)
+    }
+    
+}
