d3eb044590159719cad1d2ecf312ec0a72ef95eb
Brian Osborn <bosborn@gmail.com>
2022-08-09 13:34:33 -0600

    gars updates

--- gars-ios/features/GridLine.swift
+++ gars-ios/features/GridLine.swift
@@ -11,8 +11,127 @@ import grid_ios
 /**
  * Line between two points
  */
-class GridLine: Line {
+public class GridLine: Line {
     
+    /**
+     * Grid type the line represents if any
+     */
+    public var gridType: GridType?
+    
+    /**
+     * Initialize
+     *
+     * @param point1
+     *            first point
+     * @param point2
+     *            second point
+     */
+    public override init(point1: GridPoint, point2: GridPoint) {
+        super.init(point1: point1, point2: point2)
+    }
+    
+    /**
+     * Initialize
+     *
+     * @param point1
+     *            first point
+     * @param point2
+     *            second point
+     * @param gridType
+     *            line grid type
+     */
+    public convenience init(point1: GridPoint, point2: GridPoint, gridType: GridType?) {
+        self.init(point1: point1, point2: point2)
+        self.gridType = gridType
+    }
+    
+    /**
+     * Initialize
+     *
+     * @param line
+     *            line to copy
+     */
+    public override init(line: Line) {
+        super.init(line: line)
+    }
+    
+    /**
+     * Initialize
+     *
+     * @param line
+     *            line to copy
+     * @param gridType
+     *            line grid type
+     */
+    public convenience init(line: Line, gridType: GridType?) {
+        self.init(line: line)
+        self.gridType = gridType
+    }
+    
+    /**
+     * Initialize
+     *
+     * @param gridLine
+     *            line to copy
+     */
+    public convenience init(gridLine: GridLine) {
+        self.init(line: gridLine, gridType: gridLine.gridType)
+    }
+    
+    /**
+     * Check if the line has a grid type
+     *
+     * @return true if has grid type
+     */
+    public func hasGridType() -> Bool {
+        return gridType != nil
+    }
+    
+    public override func mutableCopy(with zone: NSZone? = nil) -> Any {
+        return GridLine(gridLine: self)
+    }
+    
+    public override func encode(with coder: NSCoder) {
+        let gridValue = gridType != nil ? gridType?.rawValue : -1
+        coder.encode(gridValue, forKey: "gridType")
+        super.encode(with: coder)
+    }
+    
+    public required init?(coder: NSCoder) {
+        let gridValue = coder.decodeInteger(forKey: "gridType")
+        if (gridValue >= 0) {
+            gridType = GridType.init(rawValue: gridValue)
+        }
+        super.init(coder: coder)
+    }
+    
+    public func isEqual(gridLine: GridLine?) -> Bool {
+        if(self == gridLine) {
+            return true
+        }
+        if(gridLine == nil) {
+            return false
+        }
+        if(!super.isEqual(gridLine)) {
+            return false
+        }
+        return true
+    }
+    
+    public override func isEqual(_ object: Any?) -> Bool {
+        
+        if(!(object is GridLine)) {
+            return false
+        }
+        
+        return isEqual(gridLine: object as? GridLine)
+    }
 
+    public override var hash: Int {
+        let prime = 31
+        var result = super.hash
+        result = prime * result + ((gridType == nil) ? -1 : gridType!.rawValue)
+        return result
+    }
     
 }
---
+++ gars-ios/grid/GridType.swift
@@ -0,0 +1,129 @@
+//
+//  GridType.swift
+//  gars-ios
+//
+//  Created by Brian Osborn on 8/9/22.
+//
+
+import Foundation
+
+/**
+ * Grid type enumeration
+ */
+public enum GridType: Int, CaseIterable {
+    
+    /**
+     * Twenty Degree
+     */
+    case TWENTY_DEGREE
+    
+    /**
+     * Ten Degree
+     */
+    case TEN_DEGREE
+    
+    /**
+     * Five Degree
+     */
+    case FIVE_DEGREE
+    
+    /**
+     * One Degree
+     */
+    case ONE_DEGREE
+    
+    /**
+     * Thirty Minute
+     */
+    case THIRTY_MINUTE
+    
+    /**
+     * Fifteen Minute
+     */
+    case FIFTEEN_MINUTE
+    
+    /**
+     * Five Minute
+     */
+    case FIVE_MINUTE
+
+    /**
+     * Get the precision in degrees
+     *
+     * @return precision degrees
+     */
+    public func precision() -> Double {
+        let precision: Double
+        switch self {
+        case .TWENTY_DEGREE:
+            precision = 20.0
+        case .TEN_DEGREE:
+            precision = 10.0
+        case .FIVE_DEGREE:
+            precision = 5.0
+        case .ONE_DEGREE:
+            precision = 1.0
+        case .THIRTY_MINUTE:
+            precision = 0.5
+        case .FIFTEEN_MINUTE:
+            precision = 0.25
+        case .FIVE_MINUTE:
+            precision = 0.25 / 3.0
+        }
+        return precision
+    }
+    
+    /**
+     * Get the precision of the value in degrees
+     *
+     * @param value
+     *            value in degrees
+     * @return precision grid type
+     */
+    public static func precision(value: Double) -> GridType {
+        let precision: GridType
+        if (value.truncatingRemainder(dividingBy: TWENTY_DEGREE.precision()) == 0) {
+            precision = TWENTY_DEGREE
+        } else if (value.truncatingRemainder(dividingBy: TEN_DEGREE.precision()) == 0) {
+            precision = TEN_DEGREE
+        } else if (value.truncatingRemainder(dividingBy: FIVE_DEGREE.precision()) == 0) {
+            precision = FIVE_DEGREE
+        } else if (value.truncatingRemainder(dividingBy: ONE_DEGREE.precision()) == 0) {
+            precision = ONE_DEGREE
+        } else if (value.truncatingRemainder(dividingBy: THIRTY_MINUTE.precision()) == 0) {
+            precision = THIRTY_MINUTE
+        } else if (value.truncatingRemainder(dividingBy: FIFTEEN_MINUTE.precision()) == 0) {
+            precision = FIFTEEN_MINUTE
+        } else {
+            precision = FIVE_MINUTE
+        }
+        return precision
+    }
+    
+    /**
+     * Get the less precise (larger precision value) grid types
+     *
+     * @param type
+     *            grid type
+     * @return grid types less precise
+     */
+    public static func lessPrecise(type: GridType) -> [GridType] {
+        let cases = self.allCases
+        let index = cases.firstIndex(of: type)
+        return Array(cases.prefix(upTo: index!))
+    }
+    
+    /**
+     * Get the more precise (smaller precision value) grid types
+     *
+     * @param type
+     *            grid type
+     * @return grid types more precise
+     */
+    public static func morePrecise(type: GridType) -> [GridType] {
+        let cases = self.allCases
+        let index = cases.firstIndex(of: type)
+        return Array(cases.suffix(from: index! + 1))
+    }
+    
+}
